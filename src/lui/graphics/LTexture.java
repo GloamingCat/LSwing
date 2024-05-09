9f7c7cea2f454e01b88f263c617fe316ee590378
GloamingCat <nightlywhiskers@gmail.com>
2024-05-08 22:42:10 -0300

    Fixing LTexture-BufferedImage convertion

--- src/lui/graphics/LTexture.java
+++ src/lui/graphics/LTexture.java
@@ -49,15 +49,16 @@ public class LTexture {
 			buffer.get(bytes);
 		}
 		image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-		int[] raster = new int[width * height];
-		for (int i = 0; i < width * height; i++) {
+		int n = width * height;
+		int[] raster = new int[n];
+		for (int i = 0; i < n; i++) {
 			int r = bytes[i*4];
 			int g = bytes[i*4+1];
 			int b = bytes[i*4+2];
 			int a = bytes[i*4+3];
 			raster[i] = (a << 24) + (r << 16) + (g << 8) + b;
 		}
-		image.setRGB(0, 0, width, height, raster, 0, 4);
+		image.setRGB(0, 0, width, height, raster, 0, width);
 	}
 	
 	public boolean isEmpty() {
@@ -117,18 +118,19 @@ public class LTexture {
 	//region Serialization
 
 	public ByteBuffer toBuffer() {
-		int n = image.getWidth() * image.getHeight();
-		ByteBuffer buffer = ByteBuffer.allocateDirect(n * 4);
-		for (int x = 0; x < image.getWidth(); x++) {
-			for (int y = 0; y < image.getHeight(); y++) {
-				int pixel = image.getRGB(x, y);
-				int i = ( x + ( y * image.getHeight() ) ) * 4;
-				buffer.put(i*4,   (byte) ((pixel & 0x00ff0000) >> 16));
-				buffer.put(i*4+1, (byte) ((pixel & 0x0000ff00) >> 8));
-				buffer.put(i*4+2, (byte) (pixel & 0x000000ff));
-				buffer.put(i*4+3, (byte) ((pixel & 0xff000000) >> 24));
+		int[] pixels = new int[image.getWidth() * image.getHeight()];
+		image.getRGB(0, 0, image.getWidth(), image.getHeight(), pixels, 0, image.getWidth());
+		ByteBuffer buffer = ByteBuffer.allocateDirect(image.getWidth() * image.getHeight() * 4);
+		for(int h = 0; h < image.getHeight(); h++) {
+			for(int w = 0; w < image.getWidth(); w++) {
+				int pixel = pixels[h * image.getWidth() + w];
+				buffer.put((byte) ((pixel >> 16) & 0xFF));
+				buffer.put((byte) ((pixel >> 8) & 0xFF));
+				buffer.put((byte) (pixel & 0xFF));
+				buffer.put((byte) ((pixel >> 24) & 0xFF));
 			}
 		}
+		buffer.flip();
 		return buffer;
 	}
 
@@ -219,13 +221,16 @@ public class LTexture {
 	
 	private static final HashMap<String, BufferedImage> loadedImages = new HashMap<String, BufferedImage>();	
 	
-	public static BufferedImage getBufferedImage(String file) {
-		BufferedImage image = loadedImages.get(file);
+	public static BufferedImage getBufferedImage(String fileName) {
+		BufferedImage image = loadedImages.get(fileName);
+		File file = new File(fileName);
 		if (image == null) {
 			try {
-				image = ImageIO.read(new File(file));
-				loadedImages.put(file, image);
-			} catch (IOException e) { }
+				image = ImageIO.read(file);
+				loadedImages.put(fileName, image);
+			} catch (IOException e) {
+				System.out.println("Couldn't find image: " + file.getAbsolutePath());
+			}
 		}
 		return image;
 	}
